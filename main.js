7945a6fea0437bbbc4190d93ca542bc594c4bf52
Andrea Parodi <andrea@parro.it>
2017-05-13 11:04:39 +0200

    app ready method

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-export default function pElectron() {
-	return 42;
+import {app} from 'electron';
+import pEvent from 'p-event';
+
+export function appReady() {
+	if (app.isReady()) {
+		return Promise.resolve();
+	}
+	return pEvent(app, 'ready');
 }
+
--- test.js
+++ test.js
@@ -1,6 +1,23 @@
-import test from 'ava';
-import pElectron from '.';
+'use strict';
+const {BrowserWindow, app} = require('electron');
+const test = require('tape-async');
+const {appReady} = require('.');
 
-test('exports a function', t => {
-	t.is(typeof pElectron, 'function');
+let win;
+
+test('exports an appReady function', async t => {
+	t.is(typeof appReady, 'function');
+});
+
+test('appReady return a promise that resolve when electron app is ready', async t => {
+	await appReady();
+	// We could create a window, because the app is ready
+	win = new BrowserWindow();
+	t.is(typeof win, 'object');
+});
+
+test('app quit', t => {
+	app.on('window-all-closed', () => app.quit());
+	t.end();
+	win.close();
 });
